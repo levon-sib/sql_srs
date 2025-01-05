5a45b5c48eaf37628842e8c0d37d060c38e1b960
levon-sib <levon.galstyan@brevo.com>
2025-01-05 20:26:59 +0000

    refacto code again and update ci!

--- app.py
+++ app.py
@@ -1,55 +1,72 @@
-import streamlit as st
-import pandas as pd
-import duckdb
 import io
+import duckdb
+import pandas as pd
+import streamlit as st
 
-st.write("""
+st.write(
+    """
 # SQL SRS
 Space Repetition System SQL practice
-""")
+"""
+)
 
-csv = '''
+CSV = """
 beverage,price
 orange juice,2.5
 expresso,2
 tea,3
-'''
+"""
 
-beverages = pd.read_csv(io.StringIO(csv))
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
+CSV2 = """
 food_item,food_price
 cookie,2.5
 chocolatine,2
 muffin, 3
-'''
+"""
 
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_STR = """
 select *
 from beverages
 cross join food_items
 """
 
-solution = duckdb.sql(answer).df()
+solution_df = duckdb.sql(ANSWER_STR).df()
 
 st.header("Enter your code:")
-query = st.text_area(label = "Votre code SQL ici", value = "select * from beverages", key = "user_input")
+query = st.text_area(
+    label="Votre code SQL ici", value="select * from beverages", key="user_input"
+)
 
-result = duckdb.query(query).df()
-st.write(f"Resultat de votre query:")
-st.dataframe(result)
+if query:
+    result = duckdb.query(query).df()
+    st.write("Resultat de votre query:")
+    st.dataframe(result)
 
-# with st.sidebar:
-#     option = st.selectbox(
-#         "What would you like to review?",
-#         ["Joins", "Groups by", "Window functions"],
-#         index=None,
-#         placeholder="Select a theme..."
-#     )
-#
-#     st.write("You selected: ", option)
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing 😕")
+
+    n_lines_difference = abs(result.shape[0] - solution_df.shape[0])
+
+    if n_lines_difference != 0:
+        st.write(
+            f"Your result has {n_lines_difference} lines difference with the solution"
+        )
+
+
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review?", ["Joins", "Groups by", "Window functions"], index=None,
+        placeholder="Select a theme...",
+    )
+
+    st.write("You selected: ", option)
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -59,7 +76,7 @@ with tab2:
     st.write("table : food items")
     st.dataframe(food_items)
     st.write("expected : ")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab3:
-    st.write(answer)
+    st.write(ANSWER_STR)
