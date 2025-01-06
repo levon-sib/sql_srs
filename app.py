deeea976fa21229722b85d0b1e056858587d7b80
levon-sib <levon.galstyan@brevo.com>
2025-01-06 20:58:34 +0000

    user input to display exercise2

--- app.py
+++ app.py
@@ -16,12 +16,18 @@ with st.sidebar:
         "What would you like to review?",
         ["cross_joins", "group_by", "window_functions"],
         index=0,
-        placeholder="Select a theme...",
+        placeholder="Select a theme :)",
     )
 
     st.write("You selected: ", theme)
     exo = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exo)
+
+    exercise_name = exo.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 
 st.header("Enter your code:")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
@@ -30,19 +36,19 @@ if query:
     result = con.execute(query).df()
     st.write("Resultat de votre query:")
     st.dataframe(result)
-#
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing 😕")
-#
-#     n_lines_difference = abs(result.shape[0] - solution_df.shape[0])
-#
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"Your result has {n_lines_difference} lines difference with the solution"
-#         )
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing 😕")
+
+    n_lines_difference = abs(result.shape[0] - solution_df.shape[0])
+
+    if n_lines_difference != 0:
+        st.write(
+            f"Your result has {n_lines_difference} lines difference with the solution"
+        )
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -55,7 +61,4 @@ with tab2:
 
 
 with tab3:
-    exercise_name = exo.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
